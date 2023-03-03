14d08690b49aeb28d71c3bca68ad00002eefaa41
knightwo <ka7110zuki@gmail.com>
2023-03-03 10:46:48 +0900

    マイグレーションのファイルを作成しました。

--- src/php/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/php/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,19 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+
             $table->id();
+
+            // ここにカラムを追加していく
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('admin_flag')->default(false);
+
+            //メール送信機能 (デフォルトはnull。利用時は別途設定が必要)
+            // $table->timestamp('email_verified_at')->nullable();
+
+            $table->rememberToken(); //ログインしているかどうかを判断するためのトークン。
             $table->timestamps();
         });
     }

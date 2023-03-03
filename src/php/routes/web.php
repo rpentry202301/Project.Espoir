b11d59ae8bdc3614d05718954c766c8918cf057d
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-03-03 10:14:05 +0900

    ItemsControllerを使用したルーティングに変更

--- src/php/routes/web.php
+++ src/php/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('', [App\Http\Controllers\ItemsController::class, 'showItems'])->name('top');
 
 Auth::routes();
 

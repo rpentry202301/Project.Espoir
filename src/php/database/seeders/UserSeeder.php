d7a58ca5caa281946eda5fffbb2d4cdcb0bb2876
knightwo <ka7110zuki@gmail.com>
2023-03-03 15:50:52 +0900

    シーダー作成、マイグレーションのエラーを修正しました。

---
+++ src/php/database/seeders/UserSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(
+            [
+                [
+                    'id'         => '1',
+                    'name'       => 'admin',
+                    'email'      => 'admin@admin',
+                    'password'   => Hash::make('adminadmin'),
+                    'admin_flag' => '1'
+                ],
+                [
+                    'id'         => '2',
+                    'name'       => '山田太郎',
+                    'email'      => 'yamada@gmail.com',
+                    'password'   => Hash::make('yamadataro'),
+                    'admin_flag' => '0'
+                ]
+            ]
+        );
+        // factory(User::class)->create([
+        //     'id'         => '1',
+        //     'name'       => 'admin',
+        //     'email'      => 'admin@admin',
+        //     'password'   => Hash::make('adminadmin'),
+        //     'admin_flag' => 'true'
+        // ]);
+
+        // factory(User::class)->create([
+        //     'id'         => '2',
+        //     'name'       => '山田太郎',
+        //     'email'      => 'yamada@gmail.com',
+        //     'password'   => Hash::make('yamadataro'),
+        //     'admin_flag' => 'false'
+        // ]);
+    }
+}

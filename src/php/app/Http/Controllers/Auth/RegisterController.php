fdc24cae43a28940ff6ca02d8dbf98931beb989f
naotomyogi <naotom.76@gmail.com>
2023-03-06 13:51:26 +0900

    会員登録のvalidationの制限範囲を変更しました。

--- src/php/app/Http/Controllers/Auth/RegisterController.php
+++ src/php/app/Http/Controllers/Auth/RegisterController.php
@@ -50,9 +50,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:30'],
-            'email' => ['required', 'string', 'email', 'max:30', 'unique:users'],
-            'password' => ['required', 'string', 'min:8'],
+            'name' => ['required', 'string', 'min:6', 'max:30'],
+            'email' => ['required', 'string', 'email', 'min:6', 'max:30', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'max:64'],
         ]);
     }
 
